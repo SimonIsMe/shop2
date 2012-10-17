0401fdb859070031689fc4e5f7c36b4e52f0e470
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2012-10-17 22:11:52 +0200

    Bootstrap

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,6 +3,7 @@ class CategoriesController < ApplicationController
   def show
     @category = Category.find(params[:id])
     @products = @category.products
+    @categories = Category.all
   end
 
 end
