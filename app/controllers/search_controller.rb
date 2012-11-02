f90eb5431a5190d065a7bbd8165c193f6374d0b7
Szymon Skrzyński <skrzynski.szymon@gmail.com>
2012-11-02 22:33:26 +0100

    Price between option

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,7 +3,16 @@ class SearchController < ApplicationController
     def index
         chceckBuyerSession
 
-        @products = Product.search(:name_cont => params[:search]).result(:distinct => true);
+        if params[:price_to] == ''
+            params[:price_to] = '9999999'
+        end
+
+        if params[:price_from] == ''
+            params[:price_from] = '0'
+        end
+
+        @products = Product.search(:name_cont => params[:search], :price_gteq => params[:price_from].to_i*100, :price_lteq => params[:price_to].to_i*100).result(:distinct => true);
     end
+
 
 end
